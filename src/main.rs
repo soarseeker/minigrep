7abf5fcd7b8fa432b0239c912fd34afe393e8cba
muni <muni.johnson@water.nv.gov>
2026-02-04 14:31:18 -0800

    minigrep return an error from run

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,18 +10,22 @@ fn main() {
         process::exit(1);
     });
     show_current_dir();
-    run(config);
+
+        if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);}
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
-    query: String,
+    _query: String,
     file_path: String,
 }
 
@@ -30,10 +35,10 @@ impl Config {
             return Err("not enough arguments");
         }
 
-        let query = args[1].clone();
+        let _query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        Ok(Config { _query, file_path })
     }
 }
 
